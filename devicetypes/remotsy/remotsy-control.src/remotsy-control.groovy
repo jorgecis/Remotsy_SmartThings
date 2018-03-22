c54a1b82e256498732935dd0f948e06a1d1d11e4
Jorge Cisneros <jorgecis@users.noreply.github.com>
2018-03-21 22:26:17 -0500

    Update license

--- devicetypes/remotsy/remotsy-control.src/remotsy-control.groovy
+++ devicetypes/remotsy/remotsy-control.src/remotsy-control.groovy
@@ -1,10 +1,10 @@
 /**
  *  Copyright 2016-2018 Remotsy 
  *
- *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  Licensed under the GNU General Public License, Version 3.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *      https://www.gnu.org/licenses/gpl-3.0.en.html
  *
  *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
